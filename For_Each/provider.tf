7261960cbd7a81d335af13a88d73772e9027d44b
unknown <ishwarjeet.s@outlook.com>
2026-02-15 22:04:05 +0530

    removed subscription

--- For_Each/provider.tf
+++ For_Each/provider.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_providers {
-      azurerm= {
-        source = "hashicorp/azurerm"
-        version = "4.1.0"
-      }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.1.0"
     }
+  }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "5d80a2ab-5490-438c-8f91-82a4fb09c0d1"
+  subscription_id = ""
 }
